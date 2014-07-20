a686f7f53b74cca1eded3ebba4b3f51ffa1ce98c
Sachin Acharya <iiit.sachin@gmail.com>
2014-07-20 19:54:38 +0530

    Initial Commit

---
+++ ldapreport2.sh
@@ -0,0 +1,24 @@
+
+
+if [ -s "output.csv" ]; then
+        rm output.csv
+        echo "Already existing output file deleted"
+fi
+echo "Creating new output file"
+
+while read line
+do
+	echo "$line:" >> output.csv
+	ldapsearch -x cn=$line | grep maildrop |  awk '{print "\011",$2}' >> output.csv
+done < groups.txt
+rm groups.txt
+echo "New output.csv file created"
+echo "Remember to use Tab as the delimiter when opening in Excel"
+echo "Thank you!"
+echo "The output file is mailed"
+
+cp output.csv /tmp/
+(uuencode /tmp/output.csv output.csv; echo "PFA the groups list along with its users") | mailx -s "Group-wise listing of LDAP users" -a "From:toadmin@yukthi.com" "cariappa.ba@yukthi.com"
+
+	
+
